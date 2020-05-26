4d9c7ba5a956632371ec0b6ddf558327d997e564
wongdarjo <bimaindramulya@gmail.com>
2020-05-26 21:20:50 +0700

    update seo needs

--- components/Header.js
+++ components/Header.js
@@ -4,13 +4,17 @@ const Header = () => {
     return (
         <Head>
             <title>Bima Indra Mulya | Front End Engineer</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta charset="UTF-8" />
             <meta name="description" content="I'am Bima Indra Mulya. Indonesian who loves sport, travel, and tech. Liked cheesy jokes sometimes, and been around 4 years working as a Front End Engineer." />
             <meta name="keywords" content="Front End Engineer, Front End Developer, Software Engineer, Indonesia, Jakarta" />
+            <meta name="author" content="John Doe" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <meta property="og:title" content="Bima Indra Mulya | Front End Engineer" />
-            <meta property="og:url" content="http://www.bimaindra.com" />
+            <meta property="og:description" content="I'am Bima Indra Mulya. Indonesian who loves sport, travel, and tech. Liked cheesy jokes sometimes, and been around 4 years working as a Front End Engineer." />
+            <meta property="og:url" content="https://bimaindra.com" />
             <meta property="og:image" content="https://media-exp1.licdn.com/dms/image/C5103AQGQlRNcn_C_Jg/profile-displayphoto-shrink_200_200/0?e=1594857600&v=beta&t=g4LUd97i2SjlGoj3APzfDnA3jljJy0_i5v3D8LNKVB0" />
-            <link rel="icon" href="./cdn/icons/favicon.png" type="image/png" sizes="16x16"></link>
+            <link rel="icon" href="./cdn/icons/favicon.png" type="image/png" sizes="16x16" />
+            <link rel="canonical" href="https://www.bimaindra.com" />
         </Head>
     )
 };
--- components/Socmed.js
+++ components/Socmed.js
@@ -5,19 +5,19 @@ const Socmed = () => {
         <div>
             <ul className={css['u-list']}>
                 <li className={css['u-list__item']}>
-                    <a className={css['u-link']} href="mailto:bimaindramulya@gmail.com"><img src="./cdn/icons/envelope-solid.svg" className={[css['u-icon'], css['u-icon__hovered']].join(' ')} /></a>
+                    <a className={css['u-link']} href="mailto:bimaindramulya@gmail.com"><img src="./cdn/icons/envelope-solid.svg" className={[css['u-icon'], css['u-icon__hovered']].join(' ')} alt="Bima Indra Mulya" /></a>
                 </li>
                 <li className={css['u-list__item']}>
-                    <a className={css['u-link']} href="http://linkedin.com/in/bimaindra" target="_blank"><img src="./cdn/icons/linkedin-brands.svg" className={[css['u-icon'], css['u-icon__hovered']].join(' ')} /></a>
+                    <a className={css['u-link']} href="http://linkedin.com/in/bimaindra" target="_blank"><img src="./cdn/icons/linkedin-brands.svg" className={[css['u-icon'], css['u-icon__hovered']].join(' ')} alt="Bima Indra Mulya" /></a>
                 </li>
                 <li className={css['u-list__item']}>
-                    <a className={css['u-link']} href="http://instagram.com/bimaindraa" target="_blank"><img src="./cdn/icons/instagram-square-brands.svg" className={[css['u-icon'], css['u-icon__hovered']].join(' ')} /></a>
+                    <a className={css['u-link']} href="http://instagram.com/bimaindraa" target="_blank"><img src="./cdn/icons/instagram-square-brands.svg" className={[css['u-icon'], css['u-icon__hovered']].join(' ')} alt="Bima Indra Mulya" /></a>
                 </li>
                 <li className={css['u-list__item']}>
-                    <a className={css['u-link']} href="http://twitter.com/bimaindraa" target="_blank"><img src="./cdn/icons/twitter-square-brands.svg" className={[css['u-icon'], css['u-icon__hovered']].join(' ')} /></a>
+                    <a className={css['u-link']} href="http://twitter.com/bimaindraa" target="_blank"><img src="./cdn/icons/twitter-square-brands.svg" className={[css['u-icon'], css['u-icon__hovered']].join(' ')} alt="Bima Indra Mulya" /></a>
                 </li>
                 <li className={css['u-list__item']}>
-                    <a className={css['u-link']} href="http://github.com/wongdarjo" target="_blank"><img src="./cdn/icons/github-square-brands.svg" className={[css['u-icon'], css['u-icon__hovered']].join(' ')} /></a>
+                    <a className={css['u-link']} href="http://github.com/wongdarjo" target="_blank"><img src="./cdn/icons/github-square-brands.svg" className={[css['u-icon'], css['u-icon__hovered']].join(' ')} alt="Bima Indra Mulya" /></a>
                 </li>
             </ul>
         </div>
