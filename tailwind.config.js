5188ee96f6ffbf7d32657b7fab546c681d7d1061
Bima Indra <bimaindramulya@gmail.com>
2023-06-12 09:24:05 +0700

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,10 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        dots: "radial-gradient(#94a3b8 1px,transparent 0)",
+        "dots-light": "radial-gradient(#94a3b8 1px,transparent 0)",
+        "dots-dark": "radial-gradient(#1e293b 1px,transparent 0)",
+        "grid-slate-light": `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='1' height='1' fill='none' stroke='%23f1f5f9'%3e%3cpath d='M0 .5H31.5V60'/%3e%3c/svg%3e")`,
+        "grid-slate-dark": `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='1' height='1' fill='none' stroke='%23475569'%3e%3cpath d='M0 .5H31.5V60'/%3e%3c/svg%3e")`,
       },
       colors: {
         "wd-dark": "#001219",
